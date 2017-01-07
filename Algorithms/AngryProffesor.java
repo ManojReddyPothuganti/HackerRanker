c0ba7cd198e033ffb5178ef80417547c04c4c3d1
Manoj <manojreddy.pothuganti@gmail.com>
2017-01-06 22:25:26 -0600

    modified

--- Algorithms/AngryProffesor.java
+++ Algorithms/AngryProffesor.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* Enter your code here. Read input  STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc =new Scanner(System.in);
             int n = sc.nextInt();
         List<String> array = new ArrayList<String>();
